5a98a18bcfb25e0f3a92e85af61ee417bb91aed6
dturner <dturner@kixeye.com>
2014-07-22 12:52:21 -0700

    updated as per changes to chassis-bootstrap

--- src/main/scala/com/kixeye/core/scala/template/TemplateConfiguration.scala
+++ src/main/scala/com/kixeye/core/scala/template/TemplateConfiguration.scala
@@ -1,20 +1,28 @@
 package com.kixeye.core.scala.template
 
-import com.kixeye.chassis.bootstrap.annotation.SpringApp
-import com.kixeye.chassis.transport.TransportConfiguration
 import org.springframework.context.annotation.{ComponentScan, Configuration}
 import com.kixeye.chassis.scala.transport.ScalaTransportConfiguration
-import com.kixeye.chassis.scala.transport.ScalaTransportConfiguration
-import com.kixeye.chassis.transport.TransportConfiguration
-import com.kixeye.chassis.bootstrap.annotation.SpringApp
 import com.kixeye.chassis.support.ChassisConfiguration
+import javax.annotation.{PreDestroy, PostConstruct}
+import com.kixeye.chassis.bootstrap.annotation.App
 
-@SpringApp(
+@App(
   name = "ScalaTemplateService",
   propertiesResourceLocation = "classpath:/template-defaults.properties",
-  configurationClasses = Array(classOf[ChassisConfiguration],classOf[ScalaTransportConfiguration],classOf[TemplateConfiguration]),
+  configurationClasses = Array(classOf[ChassisConfiguration], classOf[ScalaTransportConfiguration], classOf[TemplateConfiguration]),
   webapp = true)
 @Configuration
 @ComponentScan(basePackageClasses = Array(classOf[TemplateConfiguration]))
 class TemplateConfiguration {
+
+  @PostConstruct
+  def init() {
+    // Add some initialization logic here!!
+  }
+
+  @PreDestroy
+  def destroy() {
+    // Add some destruction code here!
+  }
+
 }
